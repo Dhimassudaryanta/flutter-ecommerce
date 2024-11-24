9d2494acfa26c7c34691655c299a02197b58b8ba
Dhimas Fadhiil Farhan Sudaryanta <dhimas@Dhimass-MacBook-Pro.local>
2024-11-24 17:41:44 +0700

    feat: add helpers, utils and constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
+import 'package:ecommerce/utils/App.dart';
 import 'package:ecommerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: DAppTheme.lightTheme,
-      darkTheme: DAppTheme.darkTheme,
-    );
-  }
 }
