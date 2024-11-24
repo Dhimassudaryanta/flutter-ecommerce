a739f8b64ad313aab9a2233e926d7ffd22eff5e8
Dhimas Fadhiil Farhan Sudaryanta <dhimas@Dhimass-MacBook-Pro.local>
2024-11-24 16:34:37 +0700

    first commit

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerce/utils/theme/custome_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class DAppTheme {
+  DAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: DTextTheme.light,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: DTextTheme.dark,
+  );
+}
